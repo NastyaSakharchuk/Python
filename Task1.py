952234ef67d50087b81a0ce0c27e5b27a59a1ba8
NastyaSakharchuk <91695190+NastyaSakharchuk@users.noreply.github.com>
2021-11-02 00:35:58 +0300

    Add files via upload

--- Task1.py
+++ Task1.py
@@ -1,26 +1,23 @@
 # задание 1 - вариант 1: Ввести три числа m, n, p. Подсчитать количество отрицательных чисел.
-
 m = int(input("Введите первое число: "))
 n = int(input("Введите второе число: "))
 p = int(input("Введите третье число: "))
 x = 0
-if m<0:
-    x+=1
-if n<0:
-    x+=1
-if p<0:
+if m < 0:
+    x += 1
+if n < 0:
+    x += 1
+if p < 0:
     x += 1
 print("Количество отрицательных чисел: ", x)
 
-
 # задание 1 - вариант 4: По номеру месяца напечатать пору года.
-
 month = int(input("Введите число месяца: "))
-if month <=2 or month ==12:
+if month <= 2 or month == 12:
     print("winter")
-elif month <=5:
+elif month <= 5:
     print("spring")
-elif month <=8:
+elif month <= 8:
     print("summer")
 else:
     print("autumn")
